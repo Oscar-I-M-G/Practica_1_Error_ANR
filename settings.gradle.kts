bcdda318308288f8014fca9a7af34e4b0a56fd11
Oscar-I-M-G <oscar.moreno4275@alumnos.udg.mx>
2024-02-25 11:58:32 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Error_ANR"
+include(":app")
+ 
